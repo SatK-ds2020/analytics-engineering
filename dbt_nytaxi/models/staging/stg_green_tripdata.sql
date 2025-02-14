4d0ad5cb782962422cb0a2300011132df2ef2ae8
Satindra Kathania <satindra.kathania@gmail.com>
2025-02-14 02:04:14 +0000

    created dbt fact tables

--- dbt_nytaxi/models/staging/stg_green_tripdata.sql
+++ dbt_nytaxi/models/staging/stg_green_tripdata.sql
@@ -1,43 +1,15 @@
-with 
+{{
+    config(
+        materialized='view'
+    )
+}}
 
-source as (
-  select * from {{source('staging','green_tripdata')}}
-     
-),
-
-renamed as (
-
-    select
-        vendorid,
-        lpep_pickup_datetime,
-        lpep_dropoff_datetime,
-        store_and_fwd_flag,
-        ratecodeid,
-        pulocationid,
-        dolocationid,
-        passenger_count,
-        trip_distance,
-            VendorID,
-              lpep_pickup_datetime ,
-              lpep_dropoff_datetime ,
-              store_and_fwd_flag ,
-              RatecodeID ,
-              PULocationID ,
-              DOLocationID ,
-              passenger_count ,
-              trip_distance ,
-              fare_amount ,
-              extra ,
-              mta_tax ,
-              tip_amount ,
-              tolls_amount ,
-              ehail_fee,
-              improvement_surcharge ,
-              total_amount ,
-              payment_type ,
-              trip_type ,
-              congestion_surcharge 
-
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
 )
 select
     -- identifiers
